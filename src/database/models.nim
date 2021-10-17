d0325dbb48853772e77e5c651a81cfbd9116739e
Hamid Bluri <hr.bolouri@gmail.com>
2021-10-17 13:10:44 -0700

    update database models

--- src/database/models.nim
+++ src/database/models.nim
@@ -3,28 +3,31 @@ import easydb
 var query: string
 
 Blueprint [queryHolder: query, postfix: "Model"]:
-    Table members:
+    Table member:
         id: int {.primary.}
         name: char[255]
+        phone_number: char[15]
 
     Table part:
         id: int {.primary.}
         name: string
 
+    Table quiz:
+        id: int {.primary.}
+        name: char[255]
+        part_id: int[ref part.id]
+        
     Table question:
         id: int {.primary.}
         quiz_id: int[ref quiz.id]
         answer: int
 
-    Table quiz:
-        id: int {.primary.}
-        name: char[255]
-        member_id: int[ref members.id]
-        part_id: int[ref part.id]
-
     Table record:
         id: int {.primary.}
         member_id: int[ref members.id]
+        quiz_id: int[ref quiz.id]
+        answer_list: char[255]
+        questions_order: string
 
 
 when isMainModule:
