50bbe0e9be557665b8dba110bc19322eaacae6fb
hamidb80 <hr.bolouri@gmail.com>
2021-10-17 15:43:03 +0330

    fix

--- src/database/models.nim
+++ src/database/models.nim
@@ -2,32 +2,29 @@ import easydb
 
 var query: string
 
-import macros
+Blueprint [queryHolder: query, postfix: "Model"]:
+    Table members:
+        id: int {.primary.}
+        name: char[255]
 
-expandMacros:
-    Blueprint [queryHolder: query, postfix: "Model"]:
-        Table members:
-            id: int {.primary.}
-            name: char[255]
+    Table part:
+        id: int {.primary.}
+        name: string
 
-        Table part:
-            id: int {.primary.}
-            name: string
+    Table question:
+        id: int {.primary.}
+        quiz_id: int[ref quiz.id]
+        answer: int
 
-        Table question:
-            id: int {.primary.}
-            quiz_id: int[ref quiz.id]
-            answer: int
+    Table quiz:
+        id: int {.primary.}
+        name: char[255]
+        member_id: int[ref members.id]
+        part_id: int[ref part.id]
 
-        Table quiz:
-            id: int {.primary.}
-            name: char[255]
-            member_id: int[ref members.id]
-            part_id: int[ref part.id]
-
-        Table record:
-            id: int {.primary.}
-            member_id: int[ref members.id]
+    Table record:
+        id: int {.primary.}
+        member_id: int[ref members.id]
 
 
 when isMainModule:
