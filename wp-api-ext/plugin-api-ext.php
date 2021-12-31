313702f7b7dcb05e4347b23422b838fc323e8aee
hamidb80 <hr.bolouri@gmail.com>
2021-12-31 14:22:33 +0330

    add settings page

--- wp-api-ext/plugin-api-ext.php
+++ wp-api-ext/plugin-api-ext.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://github.com/hamidb80/azmmonak/tree/main/wp-api-ext
  * Author: @hamidb80
  * Author URI: https://github.com/hamidb80
- * Version: 0.0.1
+ * Version: 0.0.2
  * Description: a plugin to extend wordpress API designed for https://rakhshan.com/
  */
 
@@ -17,17 +17,21 @@ define("WPS_DIRECTORY", dirname(__FILE__));
 
 // def ----------------------------------------------------
 
+# ------ hooks -------
 function WPAPIEXT_activate()
 {
-  add_option("api-token", "xxx");
+  # api token will be stored here as "WPAPIEXT_API_TOKEN"
+  add_option("WPAPIEXT_settings"); 
 }
 function WPAPIEXT_deactivate()
 {
-  delete_option("api-token");
+  delete_option("WPAPIEXT_settings");
 }
+
+// ------ utils -------
 function WPAPIEXT_isApiTokenValid($api_token)
 {
-  return $api_token == get_option("api-token");
+  return $api_token == get_option("WPAPIEXT_settings")["WPAPIEXT_API_TOKEN"];
 }
 
 function runQuery($sql)
@@ -36,6 +40,10 @@ function runQuery($sql)
   return $wpdb->get_results($sql);
 }
 
+# ------ impl -------
+
+// >>> get user info
+
 function getUserLevel($userId)
 {
   return intval(runQuery(
@@ -43,7 +51,7 @@ function getUserLevel($userId)
   )[0]->meta_value);
 }
 
-function WPAPIEXT_getName(WP_REST_Request $req)
+function WPAPIEXT_getUser(WP_REST_Request $req)
 {
   // api token in the header
   if (!WPAPIEXT_isApiTokenValid($req->get_header("api-token")))
@@ -63,15 +71,73 @@ function WPAPIEXT_getName(WP_REST_Request $req)
   return $user;
 }
 
+// >>> setting page
+// generated using: http://wpsettingsapi.jeroensormani.com/
+
+function WPAPIEXT_add_admin_menu()
+{
+  add_menu_page('wp-api-ext', 'wp-api-ext', 'manage_options', 'wp-api-ext', 'WPAPIEXT_options_page');
+}
+
+function WPAPIEXT_settings_init()
+{
+  register_setting('pluginPage', 'WPAPIEXT_settings');
+
+  add_settings_section(
+    'WPAPIEXT_pluginPage_section',
+    __('Your section description', 'set token api'),
+    'WPAPIEXT_settings_section_callback',
+    'pluginPage'
+  );
+
+  add_settings_field(
+    'WPAPIEXT_API_TOKEN',
+    __('Settings field description', 'set token api'),
+    'WPAPIEXT_API_TOKEN_render',
+    'pluginPage',
+    'WPAPIEXT_pluginPage_section'
+  );
+}
+
+function WPAPIEXT_API_TOKEN_render()
+{
+  $options = get_option('WPAPIEXT_settings');
+?>
+  <input type='text' name='WPAPIEXT_settings[WPAPIEXT_API_TOKEN]' value='<?php echo $options['WPAPIEXT_API_TOKEN']; ?>'>
+<?php
+}
+
+function WPAPIEXT_settings_section_callback()
+{
+  echo __('This section description', 'set token api');
+}
+
+function WPAPIEXT_options_page()
+{
+?>
+  <form action='options.php' method='post'>
+    <h2>wp-api-ext</h2>
+    <?php
+    settings_fields('pluginPage');
+    do_settings_sections('pluginPage');
+    submit_button();
+    ?>
+  </form>
+<?php
+}
+
 // register ----------------------------------------------------
 
 register_activation_hook(__FILE__, 'WPAPIEXT_activate');
 register_deactivation_hook(__FILE__, 'WPAPIEXT_deactivate');
 
+add_action('admin_menu', 'WPAPIEXT_add_admin_menu');
+add_action('admin_init', 'WPAPIEXT_settings_init');
+
 // http://example.com/wp-json/myplugin/v1/author/(?P\d+).
 add_action('rest_api_init', function () {
-  register_rest_route('wp_api_ext', '/getName/(?P<number>[+\w]+)', array(
+  register_rest_route('wp_api_ext', '/getUser/(?P<number>[+\w]+)', array(
     'methods' => 'GET',
-    'callback' => 'WPAPIEXT_getName',
+    'callback' => 'WPAPIEXT_getUser',
   ));
 });
